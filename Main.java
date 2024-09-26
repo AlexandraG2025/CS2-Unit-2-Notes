37d89ed250f7319c24047f1c39a5b294dd343a74
AlexandraG2025 <Alexandra.greenholtz2025@gbwl.org>
2024-09-26 12:56:04 +0000

    added scanner statements

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner; // import the scanner class
+
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Testing methods that RETURN vaules...");
+
+      //Construct a Scanner object
+      // System.in() is the computer's keyboard
+      Scanner scan = new Scanner(System.in);
+
+      //Take input with the Scanner methods
+      System.out.println("Enter some text ");
+      String imputStr = scan.nextLine();
+      System.out.println("Enter a whole number: ");
+      int imputInt = scan.nextInt();
+      System.out.println("Enter a decimal number: ");
+      double imputDouble = scan.nextDouble();
+      
+      //Handle the user imput (do something with teh values)
+      //Use it in a print statment 
+      System.out.println("You entered: " + imputStr);
+      //Use vaules in an expression
+      int sum = inputInt + inputDouble;
 
 
       
